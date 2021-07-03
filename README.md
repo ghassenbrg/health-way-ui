e2bf4e929cebf06eb37a1761000a78e65129d218
Marwen Lahmar <marwen.lahmar97@gmail.com>
2021-07-04 00:05:17 +0100

    integrate template

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Pifrontend
+# health-ui
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 12.0.4.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/pifrontend'),
+      dir: require('path').join(__dirname, './coverage/health-ui'),
       subdir: '.',
       reporters: [
         { type: 'html' },
