dd290a9eb88e3a05afe4e65bab2167b1dcaafeeb
Marwen Lahmar <marwen.lahmar97@gmail.com>
2021-07-09 00:15:48 +0100

    consult doctor timesheet

---
+++ src/app/common/models/TimeSheet.model.ts
@@ -0,0 +1,57 @@
+export class TimeSheet {
+  id: number;
+  day: string;
+  startTime: string;
+  endTime: string;
+  timings?: Timing[];
+}
+
+export class Timing {
+  time: string;
+  isSelected?: boolean;
+}
+
+export const TIME_SHEET_MOCK: any[] = [
+  {
+    id: 1,
+    day: 'Monday',
+    startTime: '2021-07-05T09:00:00',
+    endTime: '2021-07-05T16:00:00',
+  },
+  {
+    id: 2,
+    day: 'Tuesday',
+    startTime: '2021-07-06T09:00:00',
+    endTime: '2021-07-06T14:00:00',
+  },
+  {
+    id: 3,
+    day: 'Wednesday',
+    startTime: '2021-07-07T09:00:00',
+    endTime: '2021-07-07T11:00:00',
+  },
+  {
+    id: 4,
+    day: 'Thursday',
+    startTime: '2021-07-08T09:00:00',
+    endTime: '2021-07-08T16:00:00',
+  },
+  {
+    id: 5,
+    day: 'Friday',
+    startTime: '2021-08-09T11:00:00',
+    endTime: '2021-08-09T19:00:00',
+  },
+  {
+    id: 6,
+    day: 'Saturday',
+    startTime: '2021-07-10T09:00:00',
+    endTime: '2021-07-10T16:00:00',
+  },
+  {
+    id: 7,
+    day: 'Sunday',
+    startTime: '2021-07-11T09:00:00',
+    endTime: '2021-07-11T16:00:00',
+  }
+];
--- src/app/patient/booking/booking.component.ts
+++ src/app/patient/booking/booking.component.ts
@@ -1,4 +1,7 @@
+
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TimeSheet, TIME_SHEET_MOCK } from '@app/common/models/TimeSheet.model';
 
 @Component({
   selector: 'app-booking',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookingComponent implements OnInit {
 
-  constructor() { }
+  timeSheet: TimeSheet[] = TIME_SHEET_MOCK;
+  newDate: Date = new Date();
+
+  constructor(private route: Router) { }
 
   ngOnInit(): void {
+    this.prepareTimings();
   }
 
+  prepareTimings() {
+    let  minutesToAdd = 30;
+    let currentTiming: Date;
+
+    this.timeSheet.forEach(element => {
+      element.timings = [];
+      currentTiming = new Date(element.startTime);
+      // timing format HH:mm
+      element.timings.push({ time: currentTiming.toLocaleTimeString() });
+      for (let i = 0; currentTiming.toLocaleTimeString() < new Date(element.endTime).toLocaleTimeString(); i ++) {
+        currentTiming = new Date(currentTiming.getTime() + minutesToAdd * 60000);
+        element.timings.push({ time: currentTiming.toLocaleTimeString() });
+      }
+    });
+    console.log('timeSheet',this.timeSheet)
+    
+  }
+
+  chooseTiming(timeSheetId,time) {
+    this.timeSheet.find(time => time.id == timeSheetId).timings.forEach(timing => {
+      if (timing.time == time) {
+        timing.isSelected = true;
+      }
+      else {
+        timing.isSelected = false;
+      }
+    })
+  }
+
+  proceed() {
+    this.route.navigate(['/checkout']);
+  }
 }
