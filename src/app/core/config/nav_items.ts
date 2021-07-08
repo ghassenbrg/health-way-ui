4a35be6cd7ed5c162fc11761fe6d4aa99b60b98b
Marwen Lahmar <marwen.lahmar97@gmail.com>
2021-07-08 17:29:03 +0100

    menu items

--- src/app/core/config/nav_items.ts
+++ src/app/core/config/nav_items.ts
@@ -7,39 +7,88 @@ export const NAV_ITEMS: NavItem[] = [
     },
     {
         label: 'Doctors',
+        authorizedRoles: ['ROLE_ADMIN'],
         children: [
             {
                 label: 'Doctor Dashboard',
-                authorizedRoles: ['ROLE_DOCTOR'],
                 path: 'doctor-dashboard'
             },
             {
-                label: 'Search Doctor',
-                path: 'search'
+                label: 'My Available timings',
+                path: 'schedule-timings'
             },
             {
                 label: 'Appointments',
                 path: 'appointments'
             },
             {
-                label: 'Profile Settings',
-                authorizedRoles: ['ROLE_DOCTOR'],
-                path: 'doctor-profile-settings'
-            },
+                label: 'My patients',
+                path: 'my-patients'
+            }
         ]
     },
     {
         label: 'Patients',
-        authorizedRoles: ['ROLE_USER'],
+        authorizedRoles: ['ROLE_ADMIN'],
         children: [
             {
                 label: 'Patient Dashboard',
                 path: 'patient-dashboard'
             },
             {
+                label: 'Simple Search',
+                path: 'search'
+            },
+            {
+                label: 'Map Search',
+                path: 'grid-map'
+            },
+            {
                 label: 'Favourites',
                 path: 'favourites'
             }
         ]
+    },
+    {
+        label: 'Schedule Timings',
+        authorizedRoles: ['ROLE_DOCTOR'],
+        children: [
+            {
+                label: 'My Available timings',
+                path: 'schedule-timings'
+            },
+            {
+                label: 'Appointments',
+                path: 'appointments'
+            }
+        ]
+    },
+    {
+        label: 'My patients',
+        authorizedRoles: ['ROLE_DOCTOR'],
+        path: 'my-patients'
+    },
+    {
+        label: 'Search Doctor',
+        authorizedRoles: ['ROLE_PATIENT'],
+        children: [
+            {
+                label: 'Simple Search',
+                path: 'search'
+            },
+            {
+                label: 'Map Search',
+                path: 'grid-map'
+            }
+        ]
+    },
+    {
+        label: 'My Appointments',
+        authorizedRoles: ['ROLE_PATIENT'],
+        path: 'patient-dashboard'
+    },
+    {
+        label: 'Pharmacy',
+        path: 'pharmacy-search'
     }
 ];
