4235adaa9475a4c562e160c1c7f336313ca100da
Marwen Lahmar <marwen.lahmar97@gmail.com>
2021-07-09 13:46:50 +0100

    add primeng carousel in home component

--- src/app/core/components/home/home.component.ts
+++ src/app/core/components/home/home.component.ts
@@ -3,13 +3,29 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  doctors: any = [1, 2, 3, 4];
+  responsiveOptions: any = [
+    {
+      breakpoint: '1024px',
+      numVisible: 3,
+      numScroll: 3,
+    },
+    {
+      breakpoint: '768px',
+      numVisible: 2,
+      numScroll: 2,
+    },
+    {
+      breakpoint: '560px',
+      numVisible: 1,
+      numScroll: 1,
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
