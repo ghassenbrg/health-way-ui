3c05f361bc2f2b5bb6796f9c7ccbc0bb1f80e50a
Marwen Lahmar <marwen.lahmar97@gmail.com>
2021-07-09 01:12:27 +0100

    route to register page

--- src/app/common/register/register.component.ts
+++ src/app/common/register/register.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  doctorRegisterScreen: boolean;
+  
   constructor() { }
 
   ngOnInit(): void {
