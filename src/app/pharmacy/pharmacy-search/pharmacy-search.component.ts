451a5c00cb9bc93785e37415f6ef421a2f2140a7
amiira3 <amira.riahi@esprit.tn>
2021-07-17 03:59:02 +0100

    get all pharmacys by Fadhel

--- src/app/pharmacy/pharmacy-search/pharmacy-search.component.ts
+++ src/app/pharmacy/pharmacy-search/pharmacy-search.component.ts
@@ -2,6 +2,7 @@ import { City } from './../../core/models/city.model';
 import { CommonService } from './../../core/services/common.service';
 import { Component, OnInit } from '@angular/core';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { PharmacyService } from '@services-api/pharmacy.service';
 @Component({
   selector: 'app-pharmacy-search',
   templateUrl: './pharmacy-search.component.html',
@@ -10,21 +11,30 @@ import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 export class PharmacySearchComponent implements OnInit {
   selectedCity: any;
   items: any[];
-  constructor(private service : CommonService) { }
-  displayBasic:boolean=false;
-  displayBasicVacc:boolean=false;
+  pharmacies: any[];
+  pageSize: number = 5;
+  constructor(private service: CommonService, private servicesP: PharmacyService) { }
+  displayBasic: boolean = false;
+  displayBasicVacc: boolean = false;
 
   ngOnInit(): void {
-    this.service.getCities().subscribe(cities =>{
-      this.items=cities.sort((city1,city2)=>city1.name<city2.name?-1:1)
+    this.service.getCities().subscribe(cities => {
+      this.items = cities.sort((city1, city2) => city1.name < city2.name ? -1 : 1)
+
+    });
+    this.servicesP.getAll().subscribe(pharmasy => {
+      this.pharmacies = pharmasy
+      console.log(this.pharmacies);
 
     })
   }
   showPcrDialog() {
     this.displayBasic = true;
-}
-showPcrDialogVacc() {
-  this.displayBasicVacc = true;
-}
-
+  }
+  showPcrDialogVacc() {
+    this.displayBasicVacc = true;
+  }
+  loadMore() {
+    this.pageSize = this.pageSize + 2;
+  }
 }
