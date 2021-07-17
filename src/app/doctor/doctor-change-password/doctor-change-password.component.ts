e97312242d4cb540ed7277afbab8e16a7ecb1dac
Ghassen Bargougui <ghassen.brg16@gmail.com>
2021-07-17 06:53:06 +0100

    change doctor password screen

--- src/app/doctor/doctor-change-password/doctor-change-password.component.ts
+++ src/app/doctor/doctor-change-password/doctor-change-password.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { DoctorService } from '@services-api/doctor.service';
+import { ToastService } from '@services/toast.service';
+import { DoctorDashboardContainerComponent } from '../doctor-dashboard-container/doctor-dashboard-container.component';
 
 @Component({
   selector: 'app-doctor-change-password',
   templateUrl: './doctor-change-password.component.html',
-  styleUrls: ['./doctor-change-password.component.scss']
+  styleUrls: ['./doctor-change-password.component.scss'],
 })
 export class DoctorChangePasswordComponent implements OnInit {
-
-  constructor() { }
+  currentUser: any;
+  confirmPassword: string;
+  submitTouched: boolean;
+  oldPassword: string;
+  constructor(
+    private _doctorDashboard: DoctorDashboardContainerComponent,
+    private _doctorService: DoctorService,
+    private _toastService: ToastService
+  ) {}
 
   ngOnInit(): void {
+    this.currentUser = this._doctorDashboard.currentUser;
   }
 
+  submit() {
+    this.submitTouched = true;
+    if (this.currentUser.plainPassword == this.confirmPassword) {
+      this._doctorService
+        .updateDoctor(this.currentUser, this.currentUser.id)
+        .subscribe((user) => {
+          this._toastService.showSuccess(
+            'Success',
+            'your password changed successfully.'
+          );
+        });
+    }
+  }
 }
